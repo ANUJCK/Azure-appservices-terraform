2c7fa325f7f927e0914bcbbd250119d4a5fea893
Anuj <104450538+ANUJCK@users.noreply.github.com>
2023-02-16 16:04:17 +0530

    Update provider.tf

--- module/appservices/provider.tf
+++ module/appservices/provider.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "azurerm" {
-    client_id = "b7401eb6-c84b-46be-bfa0-0ff692c652fd"
-    client_secret = "ZWr8Q~fUkVyEsOcdXKX2kytL_oCXvpGHlmVNHafo"
-    subscription_id = "5a142c43-324b-4bfa-9642-608c15cc0954"
-    tenant_id = "ca3f6d08-8546-4fe4-b315-63d4119a9706"
+    client_id = "b7401eb6-c84b-46be" #example
+    client_secret = "ZWr8Q~fUkVyEsOcdXKX2kytL" #example
+    subscription_id = "5a142c43-324b-4bfa" #example
+    tenant_id = "ca3f6d08-8546-4fe4" #example
 
   features {
     
